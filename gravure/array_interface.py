895eee8bee914fbe37e8ce5f7e27185ce4d764dc
gilles <gilles>
2013-03-19 15:41:25 +0000

    * am_screen_builder.py : Make an implementation of gtk.scrollable for the MatriView widget. * array_interface : new module with definition of an ABC that map the __array_interface__ developped in numpy. It's a usefull interface for defining behavior of nDimensional array. In this module is an Adaptor Class implementing this ABC for encapsulation of type like python memoryview.

--- gravure/array_interface.py
+++ gravure/array_interface.py
@@ -19,21 +19,13 @@
 # if not, write to the Free Software Foundation, Inc., 51 Franklin St,
 # Fifth Floor, Boston, MA 02110-1301, USA.
 
-__all__ = ['MatrixView', 'CellViewer']
-
-
-import sys
-from gi.repository import Gtk
-from gi.repository import GObject
+__all__ = ['__array_interface__']
 
 from abc import *
 
-from halftone import spotfunctions
-from halftone.base import *
 
-
-class ArrayInterface(metaclass=ABCMeta):
-    """ArrayInterface.
+class __array_interface__(metaclass=ABCMeta):
+    """ABC array interface.
 
     http://docs.scipy.org/doc/numpy/reference/arrays.interface.html#\
 __array_interface__
@@ -47,7 +39,6 @@ __array_interface__
         not provided.
 
         The keys are:
-
 
         shape (required)
 
@@ -174,16 +165,15 @@ __array_interface__
         instance_dict = {}
         def __get__(self, instance, cls):
             if instance is not None:
-                return __ArrayInterface.instance_dict[id(instance)]
+                return self.__class__.instance_dict[id(instance)]
             else :
-                return AttributError()
-
+                return AttributeError()
 
     __array_interface__ = __ArrayInterface()
 
-    def __new__(cls):
+    def __new__(cls, *args, **kargs):
         self = super().__new__(cls)
-        ArrayInterface.__ArrayInterface.instance_dict[id(self)] =\
+        __array_interface__.__ArrayInterface.instance_dict[id(self)] =\
                         {'shape':NotImplemented,
                          'typestr':NotImplemented,
                          'version':3,
@@ -194,34 +184,51 @@ __array_interface__
                          'offset':0}
         return self
 
-    @abstractproperty
+    @property
+    def data(self):
+        """Python buffer object pointing to the start of the array’s data.
+        """
+        return self.__array_interface__['data']
+
+    @property
     def ndim(self):
         """Number of array dimensions.
         """
         return len(self.__array_interface__['shape'])
 
-    @abstractproperty
+    @property
     def shape(self):
         """Tuple of integers whose elements are the array size\
 in each dimension.
         """
         return self.__array_interface__['shape']
 
-    @abstractproperty
+    @property
     def itemsize(self):
         """Length of one array element in bytes.
         """
         return int(self.__array_interface__['typestr'][-1:])
 
-    @abstractproperty
+    @property
+    def nbytes(self):
+        """Total bytes consumed by the elements of the array.
+
+        Does not include memory consumed by non-element attributes
+        of the array object. For this use the __sizeof__() method.
+        """
+        return self.size * self.itemsize
+
+    @property
     def strides(self):
         """Tuple of integers giving the size in bytes to step in each\
 dimension when traversing an array.
         """
         return self.__array_interface__['strides']
 
-    @abstractproperty
+    @property
     def size(self):
+        """Number of elements in the array.
+        """
         r = 1
         for e in self.shape:
             r *= e
@@ -231,202 +238,83 @@ dimension when traversing an array.
     def tolist(self):
         return NotImplementedError
 
+    def __len__(self):
+        return self.shape[0]
+
     @classmethod
     def __subclasshook__(cls, sbcls):
-        if cls is ArrayInterface:
-            if hasattr(sbcls, '__array_interface__') or \
-               (hasattr(sbcls, 'ndim') and \
-                hasattr(sbcls, 'shape') and \
-                hasattr(sbcls, 'itemsize') and \
-                hasattr(sbcls, 'strides') and \
-                hasattr(sbcls, 'tolist')):
+        if cls is __array_interface__:
+            if hasattr(sbcls, '__array_interface__'):
                 return True
             else:
                 return False
         return NotImplemented
 
 
-import numpy as np
-ArrayInterface.register(np.ndarray)
-x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
-print('numpy ndarray have AI:', isinstance(x, ArrayInterface))
-m = memoryview(b'abcd')
-print('memoryview have AI:', isinstance(m, ArrayInterface))
-help(ArrayInterface)
+class Adapter(__array_interface__):
 
+    # x, c, s, p : not sure it's correct
+    _format = {'x':'t',
+               'c':'i',
+               'b':'i',
+               'B':'u',
+               '?':'b',
+               'h':'i',
+               'H':'u',
+               'i':'i',
+               'I':'u',
+               'l':'i',
+               'L':'u',
+               'q':'i',
+               'Q':'u',
+               'f':'f',
+               'd':'f',
+               's':'S',
+               'p':'S',
+               'P':'V'}
 
+    def __init__(self, o):
+        __array_interface__.__init__(self)
+        if isinstance(o, memoryview):
+            self.__array_interface__['shape'] = o.shape
+            typestr = '|' + Adapter._format[o.format] + str(o.itemsize)
+            self.__array_interface__['typestr'] = typestr
+            self.__array_interface__['strides'] = o.strides
+            self.__array_interface__['data'] = o
+            self.tolist = o.tolist
+        else:
+            return NotImplementedError
 
-class MatrixView(Gtk.DrawingArea, Gtk.Scrollable):
-    """A scrollable Gtk widget for making graphical representation\
-    of 2D matrix (in sense of raster object).
+    def from_memoryview(self, mem):
+        pass
 
-    Extends Gtk.DrawingArea and implements Gtk.Scrollable.
-    """
+    def from_list(self):
+        pass
 
-    hscroll_policy = GObject.property(type=Gtk.ScrollablePolicy,
-                                      default=Gtk.ScrollablePolicy.MINIMUM,
-                                      flags=GObject.PARAM_READWRITE)
-    vscroll_policy = GObject.property(type=Gtk.ScrollablePolicy,
-                                      default=Gtk.ScrollablePolicy.MINIMUM,
-                                      flags=GObject.PARAM_READWRITE)
-
-    def set_hadjustment(self, value):
-        setattr(MatrixView.hadjustment, '_property_helper_hadjustment', value)
-        self.hadjustment.set_page_increment(5)
-        self.hadjustment.set_step_increment(1)
-        self.hadjustment.connect('value-changed', self._on_hscroll)
-
-    def get_hadjustment(self):
-        return getattr(MatrixView.hadjustment, '_property_helper_hadjustment')
-
-    hadjustment = GObject.property(type=Gtk.Adjustment,
-                                   default=None,
-                                   setter=set_hadjustment,
-                                   getter=get_hadjustment,
-                                   flags=GObject.PARAM_READWRITE)
-
-    def set_vadjustment(self, value):
-        setattr(MatrixView.vadjustment, '_property_helper_vadjustment', value)
-        self.vadjustment.set_page_increment(5)
-        self.vadjustment.set_step_increment(1)
-        self.vadjustment.connect('value-changed', self._on_vscroll)
-
-    def get_vadjustment(self):
-        return getattr(MatrixView.vadjustment, '_property_helper_vadjustment')
-
-    vadjustment = GObject.property(type=Gtk.Adjustment,
-                                   default=None,
-                                   setter=set_vadjustment,
-                                   getter=get_vadjustment,
-                                   flags=GObject.PARAM_READWRITE)
-
-    hsize = GObject.property(type=int,
-                             flags=GObject.PARAM_READWRITE,
-                             default=1,
-                             minimum=1,
-                             maximum=8000)
-
-    vsize = GObject.property(type=int,
-                             flags=GObject.PARAM_READWRITE,
-                             default=1,
-                             minimum=1,
-                             maximum=8000)
-
-    scale = GObject.property(type=float,
-                             flags=GObject.PARAM_READWRITE,
-                             default=1.0,
-                             minimum=1.0)
-
-    def get_data(self):
-        return self._data
-
-    def set_data(self, value):
-        self._data = value
-
-    data = property(fget=get_data, fset=set_data)
-
-    def __init__(self, data=None):
-        Gtk.DrawingArea.__init__(self)
-        self.data = data
-        self.connect('draw', self._on_draw_cb)
-
-    def set_size(self, h, v):
-        self.hsize, self.vsize = h, v
-
-    def get_size(self):
-        return self.hsize, self.vsize
-
-    def _on_hscroll(self, adjustment):
-        self.data = 'scroll'
-        print("H scroll", adjustment.get_value())
-
-    def _on_vscroll(self, adjustment):
-        print("V scroll", adjustment.get_value())
-
-    def _on_draw_cb(self, widget, ctx):
-        w = self.get_allocated_width()
-        h = self.get_allocated_height()
-
-        self.hadjustment.set_page_size(w)
-        self.hadjustment.set_lower(1)
-        self.hadjustment.set_upper(1000)
-
-        self.vadjustment.set_page_size(h)
-        self.vadjustment.set_lower(1)
-        self.vadjustment.set_upper(1000)
-
-        print('h_page_size', self.hadjustment.get_page_size())
-        print('h_value', self.hadjustment.get_value())
-        print('h_upper', self.hadjustment.get_upper())
-        print('h_step', self.hadjustment.get_step_increment())
-
-        self._draw_background(ctx, w, h)
-        self._draw_bounding_box(ctx, w, h)
-
-        print('w,h: ', w, h)
-
-        return True
-
-    def _draw_background(self, ctx, w, h):
-        ctx.rectangle(0, 0, w, h)
-        ctx.set_source_rgba(0, 0, 0.5)
-        ctx.fill()
-
-    def _draw_bounding_box(self, ctx, w, h):
-        ctx.rectangle(50, 50, 100, 100)
-        ctx.set_source_rgba(0.5, 0.5, 0.5)
-        ctx.fill()
-
-
-class CellViewer(Gtk.Window):
-
-    def __init__(self, cell, title="Cell view", application=None):
-        if not isinstance(cell, Cell):
-            raise AttributeError("cell should be an halfone.base.Cell Type")
-        Gtk.Window.__init__(self, application=application)
-
-        # Attributes
-        self.cell = cell
-        scroll_win = Gtk.ScrolledWindow()
-        self.add(scroll_win)
-        self.draw_area = MatrixView()
-        scroll_win.add(self.draw_area)
-
-        # window setting
-        self.set_title(title)
-        self.set_default_size(400, 400)
-        self.set_position(Gtk.WindowPosition.CENTER)
-        self.show_all()
-
-
-class App(Gtk.Application):
-
-    def __init__(self):
-        Gtk.Application.__init__(self)
-        self.test()
-
-    def test(self):
-        spot_f = spotfunctions.Rhomboid()
-        self.h_cell = Cell(93, 93)
-        self.topwin = CellViewer(self.h_cell, application=self)
-        self.h_cell.setBuildOrder(BuildOrderSpotFunction(spot_f))
-        self.h_cell.fill()
-
-        print(spot_f)
-        print(self.h_cell)
-        #self.topwin.plotWhiteningOrder()
-
-    def do_activate(self):
-        self.topwin.show_all()
-
-    def do_startup(self):
-        Gtk.Application.do_startup(self)
-
-
-def main():
-    exit_code = App().run(sys.argv)
-    sys.exit(exit_code)
+    def tolist(self):
+        return None
 
 
 if __name__ == '__main__':
-    main()
+    import numpy as np
+    __array_interface__.register(np.ndarray)
+    x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+    print('numpy ndarray have AI:', isinstance(x, __array_interface__))
+    m = memoryview(b'abcd')
+    print('memoryview have AI:', isinstance(m, __array_interface__))
+    #help(__array_interface__)
+    am = Adapter(m)
+    print(am)
+    print('Adapter have AI:', isinstance(am, __array_interface__))
+    print(dir(am))
+    print(am.__array_interface__)
+    print('am.size:',  am.size)
+    print('am.strides:',  am.strides)
+    print('am.itemsize:',  am.itemsize)
+    print('am.shape:',  am.shape)
+    print('am.ndim:',  am.ndim)
+    print('am.nbytes', am.nbytes)
+    print('am.data', am.data)
+    print('len(am)', len(am))
+    print('am.tolist():',  am.tolist())
+    #help(Adapter)
