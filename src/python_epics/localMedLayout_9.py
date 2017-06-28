1bba311ef184a6ee2fc302151251460dd601669e
gilles <gilles>
2017-06-28 18:21:09 +0000

    Remove spline code examples from other project from repository.

--- src/python_epics/localMedLayout_9.py
+++
@@ -1,9 +0,0 @@
-def localMedLayout():
-   # This method must return a list of [X,Y] positions of each detector
-   # element.  The origin is in the lower left as looking at the front of the
-   # the detector, and the units are "detector widths".
-   layout = [
-         [0, 2], [1, 2], [2, 2],  # Detectors 1-3
-         [0, 1], [1, 1], [2, 1],  # Detectors 4-6
-         [0, 0], [1, 0], [2, 0]]  # Detectors 7-9
-   return(layout)
