02a32b938a8b1e2c174e19b30fd466025c2991a6
gilles <gilles>
2017-06-28 18:15:16 +0000

    Remove smc.freeimage from repository.

--- smc.freeimage-0.3dev/smc/__init__.py
+++
@@ -1,6 +0,0 @@
-# semantics namespace package 
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
