27901a966c0c59a8fa3cafbe2c6bb444791b3b2e
Gilles Coissac <gilles@ideographe.fr>
2022-06-02 13:39:19 +0200

    Remastering source tree

---
+++ src/gravure/__init__.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 Atelier Obscur.
+# Authors:
+# Gilles Coissac <gilles@atelierobscur.org>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License with
+# the Debian GNU/Linux distribution in file /usr/share/common-licenses/GPL;
+# if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+# Fifth Floor, Boston, MA 02110-1301, USA.
+import enum
+
+__all__ = ['ANGLE', 'POLARITY', 'PARAM']
+
+
+@enum.unique
+class ANGLE(enum.IntEnum):
+    """Enumeration used to set unit measurement of angle.
+
+    Valid values are : ANGLE.DEGREE and ANGLE.RADIAN.
+    """
+    DEGREE = 1
+    RADIAN = 0
+
+
+@enum.unique
+class POLARITY(enum.IntEnum):
+    """Enumeration describing how to interpret numerical values of colorant.
+
+    Valid values are : POLARITY.ADDITIVE and POLARITY.SUBSTRACTIVE.
+    """
+    ADDITIVE = 0
+    SUBSTRACTIVE = 1
+
+
+@enum.unique
+class PARAM(enum.IntEnum):
+    SYSTEM_SERVER = 10
+    SYSTEM_LIMITS = 11
+    SYSTEM_FILTERS = 12
+    SYSTEM_FONTS = 13
+    SYSTEM_COLOR = 14
+
+    DEVICE_INSTALLABLE = 20
+    DEVICE_FILTERS = 21
+    DEVICE_STATE = 22
+    DEVICE_BEHAVIOR = 23
+    DEVICE_ACCES = 24
+
+    LINE_PRINT_MODE = 30
+    LINE_PRINT_RASTER = 31
+    LINE_COLOR_CALIBRATION = 32
+    LINE_GEOMETRY_CALIBRATION = 33
+
+    JOB_SOURCE_COLOR_HANDLING = 40
+    JOB_DEST_COLOR_HANDLING = 41
+    JOB_MEDIA_SELECTION = 42
+    JOB_IMPOSITION = 43
+    JOB_FINISHING = 44
+    JOB_COLOR_ADJUSTEMENT = 45
+    JOB_GEOMETRY_ADJUSTEMENT = 46
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/gravure/tools/ppdparser.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 Atelier Obscur.
+# Authors:
+# Gilles Coissac <gilles@atelierobscur.org>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License with
+# the Debian GNU/Linux distribution in file /usr/share/common-licenses/GPL;
+# if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+# Fifth Floor, Boston, MA 02110-1301, USA.
+import os
+import fnmatch
+import string
+import xml.etree.ElementTree as etree
+from xml.dom import minidom
+
+import PARAM
+import parameters
+
+PPD_DICTIONARY_FILENAME = 'ppd_dictionary.xml'
+PPD_DICTIONARY = etree.Element('ppd_dictionary')
+etree.SubElement(PPD_DICTIONARY, 'keywords')
+etree.SubElement(PPD_DICTIONARY, 'qualifiers')
+
+
+def buildPPDDictionary(fromXML=True):
+    global PPD_DICTIONARY
+    keys_dir = getPPDDictionaryPath()
+    if os.path.exists(keys_dir) and fromXML:
+        PPD_DICTIONARY = openPPDDictionary()
+    for files in os.listdir(keys_dir):
+        if fnmatch.fnmatch(files, 'keywords_*'):
+            p = os.path.join(keys_dir, files)
+            k_dict = PPD_DICTIONARY.find('keywords')
+            try:
+                fk = open(p, 'r')
+                sp = string.rsplit(files[9:], '_', 2)
+                sp2 = string.rsplit(sp[0], '-', 2)
+                kd = {'origin': sp2[0], 'version': sp2[1], 'base_type': sp[1]}
+                for line in fk:
+                    tag = '_' + string.rstrip(line, '\n')
+                    if k_dict.find(tag) is None:
+                        etree.SubElement(k_dict, tag, kd)
+                    else:
+                        print (tag, 'already in PPD_DICTIONARY')
+                        print(files)
+            except IOError:
+                print('error when parsing data keywords files')
+            finally:
+                fk.close()
+    for files in os.listdir(keys_dir):
+        if fnmatch.fnmatch(files, 'qualifier_*'):
+            p = os.path.join(keys_dir, files)
+            q_dict = PPD_DICTIONARY.find('qualifiers')
+            try:
+                fk = open(p, 'r')
+                tag = string.rsplit(files, '_', 2)[1]
+                for line in fk:
+                    keyword = string.rstrip(line, '\n')
+                    for e in q_dict.findall(tag):
+                        if e.get('keyword') == keyword:
+                            print('qualifier already present for keyword')
+                            break
+                    else:
+                        # TODO: test if qualifiers not define inexistant keyword
+                        etree.SubElement(q_dict, tag, {'keyword': keyword})
+            except IOError:
+                print('error when parsing data qualifiers files')
+            finally:
+                fk.close()
+    writePPDDictionary(PPD_DICTIONARY)
+
+
+def writePPDDictionary(element):
+    keys_dir = getPPDDictionaryPath()
+    s = etree.tostring(element, encoding='UTF-8', method='xml')
+    p = minidom.parseString(s)
+    p = p.toprettyxml(indent="  ")
+    dp = os.path.join(keys_dir, PPD_DICTIONARY_FILENAME)
+    try:
+        df = open(dp, 'w')
+        df.write(p)
+    except IOError:
+        print('error when opening ppd dictionary')
+    finally:
+        df.close()
+
+
+def openPPDDictionary():
+    keys_dir = getPPDDictionaryPath()
+    dp = os.path.join(keys_dir, PPD_DICTIONARY_FILENAME)
+    element = None
+    try:
+        df = open(dp, 'r')
+        element = etree.fromstringlist(df.readlines())
+    except IOError:
+        print('error when opening ppd dictionary')
+    finally:
+        df.close()
+    return element
+
+
+def getPPDDictionaryPath():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(base_dir, 'keywords')
